ef048c8d5b6e1a8c9aea301202c72c1b2f746869
Dushan Chanuka <chanukadushan1030@gmail.com>
2024-07-03 00:42:58 +0530

    connect to the database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,22 @@
-import express from 'express';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
 const app = express();
 
-app.listen(3000,()=>{
-    console.log('Server running on port 3000');
-})
+dotenv.config();
+
+mongoose
+  .connect(
+    process.env.MONGO
+  )
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
